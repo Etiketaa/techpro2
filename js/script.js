bd296e2fb80b86f3d3c839360a108b0e8a171755
Franco Paredes <143425642+Etiketaa@users.noreply.github.com>
2023-11-19 13:01:58 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,18 +13,6 @@ if (nombre) {
   alert("¡Bienvenido! Gracias por visitar nuestra página.");
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  window.addEventListener('scroll', function () {
-    const article = document.querySelector('.fade-in');
-    const articleTop = article.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-
-    if (articleTop < windowHeight) {
-      article.style.opacity = 1;
-    }
-  });
-});
-
 function pantallas() {
   document.getElementById("anatomia").innerHTML = "Las pantallas";
 }
